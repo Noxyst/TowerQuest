a9ceec13a86cbd151691f7996d489f7677570a5f
Noxyst <gaetan.mth@gmail.com>
2015-01-31 15:07:45 +0100

    Create combat.h

---
+++ combat.h
@@ -0,0 +1,8 @@
+#ifndef COMBAT_H_INCLUDED
+#define COMBAT_H_INCLUDED
+
+int fCombat (Carac joueur, Carac monstre);
+int fPotion(Carac joueur);
+bool fEsquiveJ(Carac attaquant, Carac defenseur)
+
+#endif // COMBAT_H_INCLUDED
